4a42550f2ad692b9c1e397fea074ebf5ea5c7da0
Padmashri S <130970670+padmashri23@users.noreply.github.com>
2025-01-11 20:37:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
+import tensorflow as tf
 
 from PIL import Image
-import tensorflow as tf
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Flatten, Dense
-from tensorflow.keras.utils import to_categorical
 
-(X_train, y_train),(X_val, y_val) = cifar10.load_data()
-X_train = X_train / 255
-X_val = X_val / 255
+def main():
+    st.title('Cifer10 Web Classifier')
+    st.write('Upload any image that fits into one of the classes to see the prediction is correct.This is experimental.')
 
-y_train = to_categorical(y_train,10)
-y_val = to_categorical(y_val,10)
+    file=st.file_uploader('Please Upload an image',type=['jpg','png'])
+    if file:
+        image = Image.open(file)
+        st.image(image, use_container_width=True)
 
-model = Sequential([
-    Flatten(input_shape=(32,32,3)),
-    Dense(1000,activation='relu'),
-    Dense(10,activation='softmax')
+        resized_image= image.resize((32,32))
+        img_array=np.array(resized_image)/255
+        img_array=img_array.reshape((1,32,32,3))
 
-])
+        model=tf.keras.models.load_model('cifar10_model.h5')
 
-model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-model.fit(X_train,y_train,batch_size=64,epochs=10,validation_data=(X_val, y_val))
-model.save('cifar10_model.h5')
+        predictions=model.predict(img_array)
+        cifar10_classes=['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+        
+        fig, ax = plt.subplots()
+        y_pos=np.arange(len(cifar10_classes))
+        ax.barh(y_pos,predictions[0],align='center')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(cifar10_classes)
+        ax.invert_yaxis()
+        ax.set_xlabel("Probability")
+        ax.set_title('CIFAR10 Predictions')
+
+
+        st.pyplot(fig)
+
+
+
+    else:
+        st.text('You have not uploaded an image yet.')
+
+
+if __name__ == '__main__':
+    main()
